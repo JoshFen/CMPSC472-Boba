2f0af6fcf1f57d4cc2efafc3a4469bf464b54e16
JesseFentress <jessefentress246@gmail.com>
2023-04-07 12:28:33 -0400

    Token class

---
+++ Token.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+class Token:
+    
+    def __init__(self, token_type, position: int, length: int):
+        self.token_type = token_type
+        self.position = position
+        self.length = length
+        
+    def get_token_type(self):
+        return self.token_type
+    
+    def get_position(self) -> int:
+        return self.position
+    
+    def get_length(self) -> int:
+        return self.length
+
+    class TokenType(Enum):
+        BUBBLES = 'bubbles',
+        SYRUP = 'syrup',
+        FLAVOR = 'flavor',
+        DRINK = 'drink',
+        CONDITION = 'condition',
+        TEA = 'tea',
+        WITH_BUBBLES = 'withBubbles',
+        WITHOUT_BUBBLES = 'withoutBubbles',
+        SEMI_COLON = ';',
+        LEFT_BRACKET = '[',
+        RIGHT_BRACKET = ']',
+        LEFT_CURLY = '{',
+        RIGHT_CURLY = '}',
+        LEFT_PARENTHESIS = '(',
+        RIGHT_PARENTHESIS = ')',
+        QUOTATION = '"',
+        PLUS = '+',
+        MINUS = '-',
+        MULTIPLY = '*',
+        DIVIDE = '/',
+        EQUAL = '=',
+        NOT_EQUAL = '!='
+        CONDITIONAL_EQUAL = '==',
+        GREATER = '>',
+        LESS = '<',
+        GREATER_OR_EQUAL = '>=',
+        LESS_OR_EQUAL = '<='
+            
+        
